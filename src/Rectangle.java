2ab231bdd13374f0ccb4b6f7bec153b3556e0d76
guillemcrespiterrasa <guillem.cresp@estudiantat.upc.edu>
2026-09-12 08:36:03 +0200

    Versió 1

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,15 +1,15 @@
 public class Rectangle extends Shape {
-    // Els dos atributs amb '-' (privats) de la foto:
+    // l1 --> Costat 1 l2 --> Costat 2
     private double l1;
     private double l2;
 
-    // El constructor per donar-li valor a l1 i l2 (el necessites pel teu main: new Rectangle(5, 3))
+    // "El rectangle està format per l1 i l2
     public Rectangle(double l1, double l2) {
         this.l1 = l1;
         this.l2 = l2;
     }
 
-    // El mètode amb '+' (públic) de la foto:
+    // Àrea rectangle = Costat·Costat
     @Override
     public double area() {
         return l1 * l2;
--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,9 @@
-public class Square {
+public class Square extends Rectangle
+{
+    // Sabem que un cuadrat i un rectangle tenen la mateixa formula de l'àrea, per tant,
+    // podem reduïr codi, usant la mateixa formula pels dos
+    public Square(double l)
+    {
+        super(l, l);
+    }
 }
